9822c8193a1391bab30397a20c7f61bcd7916ae3
Mohan <mohan.s.harnale@gmail.com>
2026-03-13 13:12:31 +0530

    remote app deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    // BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
